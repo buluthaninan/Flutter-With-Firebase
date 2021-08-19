87850c741c4c282decfe1117fc96ffc3194d4ef3
pr0metheus00 <buluthaninan@gmail.com>
2021-08-19 16:28:30 +0300

    http added and init state addition to home_view

--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_with_firebase/net/api_methods.dart';
 import 'package:flutter_app_with_firebase/ui/add_view.dart';
 
 class HomeView extends StatefulWidget {
@@ -11,8 +12,38 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+
+  double bitcoin = 0.0;
+  double ethereum = 0.0;
+  double tether = 0.0;
+
+  @override
+  void initState(){
+    getValues();
+  }
+
+  getValues() async{
+    bitcoin = await getPrice("bitcoin");
+    ethereum = await getPrice("ethereum");
+    tether = await getPrice("tether");
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    getValue(String id , double amount){
+      if(id == "bitcoin"){
+        return bitcoin * amount;
+      }
+      else if(id == "ethereum"){
+        return ethereum * amount;
+      } else if(id == "tether"){
+        return tether * amount;
+      }
+    }
+
+
     return Scaffold(
       body: Container(
         width: MediaQuery.of(context).size.width,

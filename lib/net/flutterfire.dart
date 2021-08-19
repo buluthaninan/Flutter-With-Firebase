4422ec6fc20baeb9afc866ddecf4e0582e2744c2
pr0metheus00 <buluthaninan@gmail.com>
2021-08-19 16:04:45 +0300

    adding to the firestore

--- lib/net/flutterfire.dart
+++ lib/net/flutterfire.dart
@@ -1,4 +1,5 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 Future<bool> signIn(String email,String password) async {
@@ -11,6 +12,7 @@ Future<bool> signIn(String email,String password) async {
     return false;
   }
 }
+
 Future<bool> register(String email,String password) async {
   try{
     await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
@@ -27,3 +29,25 @@ Future<bool> register(String email,String password) async {
     return false;
     }
   }
+
+Future<bool> addCoin(String id, String amount) async{
+  try{
+    String uid = FirebaseAuth.instance.currentUser.uid;
+    var value = double.parse(amount);
+    DocumentReference documentReference = FirebaseFirestore.instance.collection("users").doc(uid).collection("coins").doc(id);
+
+    FirebaseFirestore.instance.runTransaction((transaction) async {
+      DocumentSnapshot snapshot = await transaction.get(documentReference);
+      if(!snapshot.exists){
+        documentReference.set({"Amount": value});
+        return true;
+      }
+      double newAmount = snapshot.data()["Amount"] + value;
+      transaction.update(documentReference, {"Amount": newAmount});
+      return true;
+    });
+  }
+  catch(e){
+    return false;
+  }
+}

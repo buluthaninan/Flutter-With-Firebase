775e3e8029d994b368ff1f120d4119b24c99843b
pr0metheus00 <buluthaninan@gmail.com>
2021-08-19 16:43:14 +0300

    bug fixed

--- lib/net/api_methods.dart
+++ lib/net/api_methods.dart
@@ -5,10 +5,10 @@ import 'dart:convert';
 
 Future<double> getPrice(String id) async {
   try{
-    var url = "https://api.coingecko.com/api/v3/coins/bitcoin/" + id;
+    var url = "https://api.coingecko.com/api/v3/coins/" + id;
     var response = await http.get(url);
     var json = jsonDecode(response.body);
-    var value = json["market_data"]["current_price"]["try"].toString();
+    var value = json['market_data']['current_price']['try'].toString();
     return double.parse(value);
   } catch (e){
     print(e.toString());
